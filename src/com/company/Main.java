58519acab41a1af78d68b012b91c085d88c66ac7
liyan <760272432@qq.com>
2017-11-29 15:08:57 +0800

    3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.printf("哈哈");
+        System.out.println("Main.main");
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+        System.out.println();
     }
 }
